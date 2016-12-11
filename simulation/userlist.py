c02d0807cf7369948146e19296560f47a021b28a
Bryant Eisenbach <fubuloubu@gmail.com>
2016-12-10 21:00:11 -0500

    Major changes so that we can set gridsize

--- simulation/userlist.py
+++ simulation/userlist.py
@@ -2,11 +2,10 @@
 from random import random
 from point import Point
 
-GRIDSIZE = Point(x=100.0, y=100.0) #feet
-
-def generateUserList(numUsers):
-    newLocation = lambda: Point(x=random()*GRIDSIZE.x, y=random()*GRIDSIZE.y)
-    return [(uid, newLocation()) for uid in range(numUsers)]
+def generateUserList(args):
+    gridsize = Point(x=args.x, y=args.y)
+    newLocation = lambda: Point(x=random()*gridsize.x, y=random()*gridsize.y)
+    return [(uid, newLocation()) for uid in range(args.users)]
 
 def readUserList(filename):
     
@@ -23,17 +22,30 @@ def readUserList(filename):
 
     return [readUser(line) for line in lines]
 
-def writeUser(user):
-    uid = user[0]
-    location = user[1]
-    return "{},{},{}".format(uid, location.x, location.y)
+def writeUserList(userList):
+
+    def writeUser(user):
+        uid = user[0]
+        location = user[1]
+        return "{},{},{}".format(uid, location.x, location.y)
+    
+    return "\n".join([ writeUser(u) for u in userList ])
+
+from argLimitedFloat import Range
+def addArgs(parser):
+    parser.add_argument('-x', metavar='M', type=int, default=100,
+        choices=[Range(10,1000)], help='The x-axis length M of the simulation grid')
+    parser.add_argument('-y', metavar='N', type=int, default=100,
+        choices=[Range(10,1000)], help='The y-axis length M of the simulation grid')
 
 if __name__ == '__main__':
     import argparse
-    from argLimitedFloat import Range
-    parser = argparse.ArgumentParser(description='Create a randomized collection of N users, and print to a file')
-    parser.add_argument('users', metavar='N', type=int,
+    parser = argparse.ArgumentParser(description='Create a randomized collection of some users '
+        'on a MxN grid, and print to a file')
+    addArgs(parser)
+    parser.add_argument('users', metavar='users', type=int,
         choices=[Range(10,10000)], help='The number of users in the simulation')
     args = parser.parse_args()
-    
-    print( "\n".join([ writeUser(u) for u in generateUserList(args.users) ]))
+    userList = generateUserList(args)
+
+    print(writeUserList(userList))
