f38cba76f9d6d320fe876e1080aa9b3521761070
Bryant Eisenbach <fubuloubu@gmail.com>
2016-12-11 00:05:25 -0500

    Added some rules to work with usergraphs

--- simulation/simulation.mk
+++ simulation/simulation.mk
@@ -1,3 +1,15 @@
+# View user graphs
+.PHONY: %-view
+%-view: %.ul
+	@echo "  VIEW $^"
+	@./userlist.py -F $^ --show-usergraph 2> /dev/null
+
+# Create user graphs
+%-usergraph.tex: %.ul
+	@echo " GRAPH $^"
+	@./userlist.py -F $^ --print-usergraph
+	@mv output-graph.tex $@
+
 # Clean up simulation
 .PHONY: clean
 clean:
