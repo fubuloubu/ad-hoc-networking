b03531c2d31380cd617a0aea5c60a47856bd6fb0
Bryant Eisenbach <fubuloubu@gmail.com>
2016-12-11 00:44:49 -0500

    Added rules for running simulations

--- simulation/simulation.mk
+++ simulation/simulation.mk
@@ -4,11 +4,29 @@
 	@echo "  VIEW $^"
 	@./userlist.py -F $^ --show-usergraph 2> /dev/null
 
-# Create user graphs
+# Print user graphs
 %-usergraph.tex: %.ul
 	@echo " GRAPH $^"
 	@./userlist.py -F $^ --print-usergraph
 	@mv output-graph.tex $@
+
+# Run simulations
+# NOTE: if %.args file exist, run N simulations
+# using arguments from the file, 1 sim per line
+%.results: %.ul
+	@echo "   RUN $^"
+	@if [ -e $*.args ] ; then \
+	 	while read -r line; \
+			do ./simulation.py $$line -F $^ >> $@ ; \
+	 	done < $*.args ; \
+	 else \
+	 	./simulation.py -F $^ >> $@ ; \
+	 fi
+
+# Process results
+%-results.tex: %.results
+	@echo "   GEN $@"
+	@./simulation_results_parser.py -t $^ > $@
 
 # Clean up simulation
 .PHONY: clean
